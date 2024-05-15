b5cd0d067002c0f66b587ae66697fe812f987eb5
Marwan Hisham <marwanhiisham@outlook.com>
2024-05-15 23:29:03 +0300

    radio-fixed

--- src/app/create-product/page.tsx
+++ src/app/create-product/page.tsx
@@ -142,7 +142,6 @@ const CreateProductPage = () => {
                   <FormControl>
                     <RadioGroup
                       onValueChange={field.onChange}
-                      defaultValue={false}
                       className="flex flex-col space-y-1"
                     >
                       <FormItem className="flex items-center space-x-3 space-y-0">
